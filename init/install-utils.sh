4d4ae1eaa1d7cdf3cd669483e0c34121a0cdfc6c
xanagit <apps@ridereau.fr>
2025-09-24 20:05:39 +0200

    🐛 Fixing mise configuration

--- init/install-utils.sh
+++ init/install-utils.sh
@@ -4,6 +4,7 @@
 ../utilities/eza.sh
 ../utilities/fuzzy-finder.sh
 ../utilities/gh-cli.sh
+../utilities/mise.sh
 ../utilities/neovim.sh
 ../utilities/ollama.sh
 ../utilities/terraform.sh
--- utilities/mise.sh
+++ utilities/mise.sh
@@ -2,3 +2,7 @@
 
 # Languages version management and local env variable
 curl https://mise.run | sh
+
+echo "eval \"\$(/home/naemider/.local/bin/mise activate bash)\"" >> ~/.bashrc
+
+source $HOME/.bashrc
