473627a3a93b2eed115fff473b062ef2edc10c1b
xanagit <apps@ridereau.fr>
2025-10-02 19:37:29 +0200

    ➕ Add kubectl bash completion

--- utilities/kubectl.sh
+++ utilities/kubectl.sh
@@ -5,3 +5,7 @@ curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stabl
 sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
 
 rm kubectl
+
+sudo apt-get install bash-completion
+echo 'source <(kubectl completion bash)' >>~/.bashrc
+
